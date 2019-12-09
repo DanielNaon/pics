0d3d1a57a5b650eead0f2e36c19d1bb47771e716
daniel naon <danielnaon@gmail.com>
2019-12-09 14:00:41 +0200

    forth

--- src/App.js
+++ src/App.js
@@ -11,20 +11,17 @@ class App extends Component{
   constructor(){
     super()
     this.state = {
-      classname: true   
+ 
     }
   }
   searchPhoto=async(searchWords)=>{
     let pictureData = await axios.get("https://pixabay.com/api/?key=14523380-031f8673b5e686b36d689adf0&q=" + searchWords + "&image_type=photo")
     let id = 1
     
-    
-    pictureData.data.hits.forEach(element => {
-      
+    pictureData.data.hits.forEach(element => { 
       let picture= element.largeImageURL
       let picId= element.id
       let picDescription= element.tags
-      
       this.props.picstore.addPicture(picture, picId, picDescription)
 
     });
@@ -33,15 +30,9 @@ class App extends Component{
     console.log(event.target.value)
     this.props.picstore.DeleteFromFaves(event.target.value)
   }
-  changeClassName=()=>{
-    this.setState({classname: this.state.classname}, function(){
-      console.log(this.state.classname)
-    })
-  }
+
   render(){
-
     let store = this.props.picstore
-
     return(
       <div className="App">
 
@@ -56,7 +47,7 @@ class App extends Component{
 
         <div className="favouriteContainer"> 
           <div className="description1">You have {store.numberOfFavePictures} favourite pics</div>
-          {store.faveuritrsPictures.map(m=><div className="faves"><img onClick={this.changeClassName}className={this.state.classname === true ?"favepics" : "favepicsbig"} src={m.picture}/><br></br>{m.picDescription}<br></br><button className="deleteFromFave" value={m.picId} onClick={this.deletePhotoFromFaveurites}>delete from faveurites</button></div>)}
+          {store.faveuritrsPictures.map(m=><div className="faves"><img onClick={this.changeClassName} className="favepics" src={m.picture}/><br></br>{m.picDescription}<br></br><button className="deleteFromFave" value={m.picId} onClick={this.deletePhotoFromFaveurites}>delete from faveurites</button></div>)}
        </div>
       </div>
     )
